c411f2449ea001f2506091fd314b5ba42b2961f6
ideal <idealities@gmail.com>
2016-06-30 20:35:59 +0800

    add encode of websocket frame

--- drummer/frame.py
+++ drummer/frame.py
@@ -19,8 +19,66 @@
 #
 #
 
+import struct
+import StringIO
+
+OPCODE_CONT  = 0x0
+OPCODE_TEXT  = 0x1
+OPCODE_BIN   = 0x2
+# 0x3 - 0x7 reserved
+OPCODE_CLOSE = 0x8
+OPCODE_PING  = 0x9
+OPCODE_PONG  = 0xA
+# 0xB - 0xF reserved
+
 def encode(**args):
-    pass
+    """
+    Encode data into a valid websocket frame.
+
+    Valid args:
+
+    fin: 0 or 1, indicates whether or not this is final fragment
+    opCode: type of payloadData
+    payloadData: the real data to send
+    needMask: true, or false, default true, whether not this frame need mask
+
+    For more information, please refer to: https://tools.ietf.org/html/rfc6455
+
+    """
+    mask = (0 if 'needMask' in args and args['needMask'] == False else 1)
+    buf = StringIO.StringIO()
+    buf.write(struct.pack('B', (args['fin'] << 7) + args['opCode']))
+
+    length = len(args['payloadData'])
+    if length < 126:
+        buf.write(struct.pack('B', (mask << 7) + length))
+    elif (length < 0x10000):
+        buf.write(struct.pack('B', (mask << 7) + 126))
+        buf.write(struct.pack('>H', length))
+    else:
+        buf.write(struct.pack('B', (mask << 7) + 127))
+        buf.write(struct.pack('>Q', length))
+
+    if mask:
+        import random
+        maskList = [ random.randint(0, 255) for i in range(4) ]
+        maskList = [ 0x37, 0xfa, 0x21, 0x3d ]
+        for m in maskList:
+            buf.write(struct.pack('B', m))
+        i = 0
+        while i < length:
+            buf.write(struct.pack('B',
+                struct.unpack('B', args['payloadData'][i])[0] ^ maskList[i % 4]))
+            i += 1
+    else:
+        buf.write(args['payloadData'])
+
+    buf.seek(0)
+    content = buf.read()
+    buf.close()
+
+    return content
 
 def decode(data):
     pass
+
